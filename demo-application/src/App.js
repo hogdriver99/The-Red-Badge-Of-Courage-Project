4495469ac63548f3e69e385c40ab58483efd87d5
Olena Buriakova <olena.buriakova@gmail.com>
2022-03-03 16:36:46 -0500

    Added responsive menu bar

--- demo-application/src/App.js
+++ demo-application/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Book from './components/Book';
 import ControlPanel from './components/ControlPanel';
 import Menu from './components/Menu';
+import MenuMobile from './components/MenuMobile';
 
 function App() {
   return (
     <div className="app" id="app">
       <Menu />
+      <MenuMobile />
       <Book />
       <ControlPanel />
     </div>
---
+++ demo-application/src/components/MenuMobile.js
@@ -0,0 +1,26 @@
+import nav from '../images/nav.png'
+import home from '../images/home.png'
+import Icon from './Icon'
+import Button from './Button'
+
+const Menu = () => {
+    return (
+        <div className='menuMobile' style={menuStyle}>
+
+            <h1 id='h1small'>The Red Badge of Courage</h1>
+            <div className='menuMobileRow'>
+                <div>
+                    <Icon image={nav} name='nav'/>
+                    <Icon image={home} name='home'/>
+                </div>
+                <Button text='Log In'/>
+            </div>
+        </div>
+    )
+}
+
+export default Menu
+
+const menuStyle = {
+    height: '70px',
+}
