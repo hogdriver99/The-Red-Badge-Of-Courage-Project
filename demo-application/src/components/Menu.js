c10d37fc8a080c1f5f535a36b4e38b206393d5ba
Olena Buriakova <olena.buriakova@gmail.com>
2022-03-05 17:47:01 -0500

    Fixed resising detection

--- demo-application/src/components/Menu.js
+++ demo-application/src/components/Menu.js
@@ -3,27 +3,51 @@ import home from '../images/home.png'
 import Icon from './Icon'
 import Button from './Button'
 import NavSideBar from './NavSideBar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Menu = () => {
-    const [showNavBar, setShowNavBar] = useState(true)
+    // Showing Mobile Version
 
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            console.log("RESIZE")
+            if (window.innerWidth > 800) {
+                setShowMobileMenu(false)
+                console.log("Web")
+            } else {
+                setShowMobileMenu(true)
+                console.log("Mobile")
+            }
+        };
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // Showing the navigation side bar
+    const [showNavBar, setShowNavBar] = useState(true)
     const navOnClick = () => {
         setShowNavBar(!showNavBar)
-        console.log("CLICK", showNavBar)
+        console.log(window.innerWidth)
     }
 
     return (
         <>
             <div className='menu' style={menuStyle}>
+                {showMobileMenu && <Icon image={nav} name='nav' onClick={navOnClick}/>}
                 <div>
-                    <Icon image={nav} name='nav' onClick={navOnClick}/>
-                    <Icon image={home} name='home'/>
+                    {!showMobileMenu && <Icon image={nav} name='nav' onClick={navOnClick}/>}
+                    {!showMobileMenu && <Icon image={home} name='home'/>}
                 </div>
-                <h1>The Red Badge of Courage</h1>
-                <Button text='Log In'/>
+                {!showMobileMenu && <h1>The Red Badge of Courage</h1>}
+                {showMobileMenu && <h1 id='h1small'>The Red Badge of Courage</h1>}
+                {!showMobileMenu && <Button text='Log In'/>}
             </div>
-            {showNavBar && <NavSideBar show={true}/>}
+            {showNavBar && <NavSideBar mobile={showMobileMenu}/>}
         </>
         
     )
--- demo-application/src/components/NavSideBar.js
+++ demo-application/src/components/NavSideBar.js
@@ -3,31 +3,17 @@ import Button from './Button';
 import Icon from './Icon';
 import home from '../images/home.png'
 
-const NavSideBar = (show) => {
-    // if (show === true) {
-        console.log("works", show)
-        return (
-            <div className='navSideBar'>
-                <div className='navContainer'>
-                    <Icon image={home} name='home'/>
-                    <Button text='Log In'/>
-                    <h1>Item</h1>
-                </div>
+const NavSideBar = ({mobile}) => {
+    return (
+        <div className='navSideBar'>
+            <div className='navContainer'>
+                {mobile && <Icon image={home} name='home'/>}
+                {mobile && <Button text='Log In'/>}
+                <h1>Item</h1>
             </div>
-          )
-    // } 
-    // else {
-    //     return (
-    //         <></>
-    //     )
-    // }
-  
+        </div>
+        )
 };
 
-// const navStyle = {
-//     height: '40px',
-//     width: '40px',
-//     margin: '10px',
-// }
 
 export default NavSideBar;
